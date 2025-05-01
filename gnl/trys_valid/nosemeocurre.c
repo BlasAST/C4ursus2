16e695bc6d40327c898c7e17477500f2b4510ba0
BlasAST <bsiguenc@c2r10s2.42madrid.com>
2025-05-01 17:14:05 +0200

    trys gnl

--- gnl/trys_valid/nosemeocurre.c
+++ gnl/trys_valid/nosemeocurre.c
@@ -12,13 +12,13 @@ char	*get_next_line(int fd);
 char    *ft_clean_buffer(char *c);
 #endif
 
-/* 
+/*
 	* Quiero que lea linea por linea
 	* Crear un buffer que crecerá si no encuentra un \n
 	* Devolver la lectura hasta encontrar un \n
 	* Almacenar en static lo que quedo del ultimo buffer
 	* Hacer que vaya creciendo el buffer que teniamos
-	* 
+	*
  */
 
 void	ft_print_buffer(char *str)
@@ -58,6 +58,8 @@ char	*ft_realloc (char *last_buffer, char *str, int z)
 	ft_strcpy(last_buffer, buffer_temp);
 	free (buffer_temp);
 	last_buffer[size + 1] = '\0';
+	if (*str)
+		ft_strcpy(&(last_buffer[size + 1]), str);
 	return (last_buffer);
 }
 
@@ -72,7 +74,7 @@ int	ft_find_jump (char *str)
 	return (0);
 }
 
-char	*get_next_line(int fd)
+/* char	*get_next_line(int fd)
 {
 	static	char	*last_buffer;
 	char	*buffer;
@@ -87,7 +89,8 @@ char	*get_next_line(int fd)
 		free(buffer);
 		return (NULL);
 	}
-	last_buffer = ft_realloc(last_buffer, buffer, size);
+	if (!last_buffer)
+		last_buffer = ft_realloc(last_buffer, buffer, size);
 	ft_strcpy(last_buffer, buffer);
 	while (!ft_find_jump(buffer))
 	{
@@ -98,16 +101,52 @@ char	*get_next_line(int fd)
 	}
 	free(buffer);
 	return (last_buffer);
+} */
+
+char	*get_next_line(int fd)
+{
+	static char *last_buffer;
+	char	*buffer;
+	char *return_buffer;
+	int	bytes;
+	int size;
+
+	size = 1;
+	// Asignar espacio a buffer
+	buffer = malloc (BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	// Leer con el buffer
+	bytes = read(fd, buffer, BUFFER_SIZE);
+	// Si el buffer a encontrado el final trabajar el ultimo buffer
+	if (bytes <= 0)
+		return (NULL);
+	// Asignar espacio a last_buffer y copiar buffer
+	last_buffer = ft_realloc(last_buffer, buffer, size);
+	// Buscar  \N
+	// Si no se encuentra repetir
+	// Asignar lo anterior a la variable estatica + size
+	// realizar otra lectura para repetir
+	// Si la lectura encuentra el final trabajar el ultimo buffer DEVOLVERLO A RETURN BUFFER
+	// Si se detiene por el bucle devolver todo lo del buffer statico hasta el  \n
+	// Se desplaza la static hasta el siguiente sin el  \n
+	// Que pasa con el \n si se llama en la siguiente iteración?
+	// retornar return buffer
+	
+
+	
+
 }
 
 int main(void)
 {
-	int value = open("el_quijote.txt", O_RDONLY);
+	int value = open("../el_quijote.txt", O_RDONLY);
 	// int value = open("nose.txt", O_RDONLY);
 	char *c;
-	/* while ((c = get_next_line(value)))
+	while ((c = get_next_line(value)))
 	{
-	}*/
+		ft_print_buffer(c);
+	}
 
-	ft_print_buffer(get_next_line(value));
+	// ft_print_buffer(get_next_line(value));
 }
