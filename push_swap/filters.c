8784cf22c800fa0c1d757a4a2288b207bcaf6e48
BlasAST <145365170+BlasAST@users.noreply.github.com>
2025-06-27 15:57:33 +0200

    Algoritms order

--- push_swap/filters.c
+++ push_swap/filters.c
@@ -3,23 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   filters.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bsiguenc <bsiguenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 15:39:18 by marvin            #+#    #+#             */
-/*   Updated: 2025/06/26 16:01:46 by marvin           ###   ########.fr       */
+/*   Updated: 2025/06/27 15:25:19 by bsiguenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_filter_swap(int	*a)
+int	ft_filter_swap(int	*a, int len)
 {
 	int	i;
+	int	size;
 	int	temp;
 
 	i = 1;
+	size = ft_len_arr(a);
+	if (size < len)
+		return (1);
 	temp = a[0];
-	while (i < ft_len_arr(a))
+	while (i < size)
 	{
 		if (temp > a[i])
 			return (1);
@@ -27,4 +31,23 @@ int	ft_filter_swap(int	*a)
 		i++;
 	}
 	return (0);
+}
+
+int	ft_find_smaller(int *arr, int *smaller)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (arr[i])
+	{
+		if (*smaller > arr[i])
+		{
+			*smaller = arr[i];
+			j = i;
+		}
+		i++;
+	}
+	return (j);
 }
--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: bsiguenc <bsiguenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 16:02:55 by bsiguenc          #+#    #+#             */
-/*   Updated: 2025/06/25 14:23:27 by bsiguenc         ###   ########.fr       */
+/*   Updated: 2025/06/27 15:41:45 by bsiguenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,87 @@ int	main(int argn, char **args)
 {
 	int	*a;
 	int	*b;
+	int	i;
+	int	smaller;
 
 	argn -= 1;
 	a = ft_putargs(argn, args);
 	b = (int *) malloc (sizeof(int) * (argn));
 	if (!b)
 		return (0);
-	// show(a,b,argn);
-	// push_b(b, a,argn);
-	// push_b(b, a,argn);
-	// push_b(b, a,argn);
-	// show(a,b,argn);
-	// reverse_rotate_ab(a,b,argn);
-	// show(a,b,argn);
-	while (ft_filter_swap(a) == 1)
-	{
-		push_b(b,a,argn);
-		show(a,b,argn);
-	}
+	i = 0;
+	
 	return (0);
 }
+
+/* CODIGO VALIDO 1.1 con indice:
+while (ft_filter_swap(a, argn) == 1)
+{
+	if (ft_len_arr(a) != 0)
+	{
+		smaller = a[0];
+		smaller = ft_find_smaller(a, &smaller);
+		while (i != smaller)
+		{
+				rotate_a(a, argn);
+			i++;
+		}
+		i = 0;
+		push_b(b, a, argn);
+	}
+	else
+	{
+		while (i < argn)
+		{
+			push_a(a,b,argn);
+			i++;
+		}
+	}
+	// show(a,b,argn);
+} */
+
+/*  CODIGO VALIDO 1:
+
+	while (ft_filter_swap(a, argn) == 1)
+	{
+		if (ft_len_arr(a) != 0)
+		{
+			smaller = a[0];
+			smaller = ft_find_smaller(a, &smaller);
+			while (a[0] != smaller)
+			{
+				rotate_a(a, argn);
+			}
+			push_b(b, a, argn);
+		}
+		else
+		{
+			while (i < argn)
+			{
+				push_a(a,b,argn);
+				i++;
+			}
+		}
+		// show(a,b,argn);
+	} */
+
+/*	CODIGO VALIDO 2
+	while (ft_filter_swap(a, argn) == 1)
+	{
+		if (a[0] > a[1] && a[1])
+			rotate_a(a, argn);
+		else
+		{
+			push_b(b,a, argn);
+		}
+		if (ft_len_arr(a) == 0)
+		{
+			while (i < argn)
+			{
+				push_a(a,b,argn);
+				i++;
+			}
+			i = 0;
+		}
+		show(a,b,argn);
+	} */
